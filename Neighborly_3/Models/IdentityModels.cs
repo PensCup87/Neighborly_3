386bf250168294a23b0c844e7656b909d10e8cc4
PensCup87 <bdt9123@gmail.com>
2017-12-01 13:40:31 -0500

    added street address

--- Neighborly_3/Models/IdentityModels.cs
+++ Neighborly_3/Models/IdentityModels.cs
@@ -9,7 +9,7 @@ namespace Neighborly_3.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        //public string StreetAddress { get; set; }
+        public string StreetAddress { get; set; }
 
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
--- Neighborly_3/Models/TaskModel.Context.cs
+++ Neighborly_3/Models/TaskModel.Context.cs
@@ -28,5 +28,10 @@ namespace Neighborly_3.Models
         public virtual DbSet<Message> Messages { get; set; }
         public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<Task2> Task2 { get; set; }
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
     }
 }
